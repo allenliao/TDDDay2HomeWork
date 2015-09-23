15c01f942a0c2bd3788da7044e59a3edcf32631d
allenliao <allenliao@gmail.com>
2015-09-23 20:09:38 +0800

    1.add Test Project

---
+++ ShoppingCart/ShoppingCartTests/CartTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ShoppingCart.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void CalPriceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
