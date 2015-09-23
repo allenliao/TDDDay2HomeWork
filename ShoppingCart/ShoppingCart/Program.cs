587149d6aaf55e7186f6d2a0a791c3426a1d98db
allenliao <allenliao@gmail.com>
2015-09-23 20:06:25 +0800

    1.加入程式結構 2.加入測試Project

--- ShoppingCart/ShoppingCart/Program.cs
+++ ShoppingCart/ShoppingCart/Program.cs
@@ -12,4 +12,36 @@ namespace ShoppingCart
         {
         }
     }
+
+    public class Cart { 
+        public void AddBook(Book book){
+            _order.AddBook(book);
+        }
+        private Order _order;
+        public Order getOrder(){
+
+            return _order;
+        }
+        public decimal CalPrice()
+        {
+            decimal price = 0 ;
+
+            return price;
+        }
+    }
+
+
+
+    public class Order{
+        private List<Book> _bookList;
+
+        public void AddBook(Book book){
+            _bookList.Add(book);
+        }
+    }
+
+    public class Book{
+        public string bookName;
+        public decimal price=100;
+    }
 }
